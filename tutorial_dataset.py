206bc7431b3ff55038afd3cccc788599b9f53ca6
hson122 <hson122.learn@gmail.com>
2026-04-27 10:41:29 +0700

    update for tongji dataset

--- tutorial_dataset.py
+++ tutorial_dataset.py
@@ -1,16 +1,19 @@
 import json
 import cv2
 import numpy as np
-
+import os
 from torch.utils.data import Dataset
 
 
 class MyDataset(Dataset):
-    def __init__(self):
+    def __init__(self, data_dir):
         self.data = []
-        with open('./training/fill50k/prompt.json', 'rt') as f:
-            for line in f:
-                self.data.append(json.loads(line))
+        self.data_dir = data_dir
+
+        json_path = os.path.join(self.data_dir, 'prompt.json')
+
+        with open(json_path, 'rt', encodeing= 'utf-8') as f:
+                self.data = json.load(f)
 
     def __len__(self):
         return len(self.data)
@@ -18,13 +21,19 @@ class MyDataset(Dataset):
     def __getitem__(self, idx):
         item = self.data[idx]
 
-        source_filename = item['source']
-        target_filename = item['target']
+        source_filename = item['label']
+        target_filename = item['palm']
         prompt = item['prompt']
 
-        source = cv2.imread('./training/fill50k/' + source_filename)
-        target = cv2.imread('./training/fill50k/' + target_filename)
+        source_path = os.path.join(self.data_dir, 'label', source_filename)
+        target_path = os.path.join(self.data_dir, 'palm', target_filename)
 
+        source = cv2.imread(source_path)
+        target = cv2.imread(target_path)
+
+        image_size = (512, 512)
+        source = cv2.resize(source, image_size)
+        target = cv2.resize(target, image_size)
         # Do not forget that OpenCV read images in BGR order.
         source = cv2.cvtColor(source, cv2.COLOR_BGR2RGB)
         target = cv2.cvtColor(target, cv2.COLOR_BGR2RGB)
--- tutorial_train.py
+++ tutorial_train.py
@@ -5,15 +5,34 @@ from torch.utils.data import DataLoader
 from tutorial_dataset import MyDataset
 from cldm.logger import ImageLogger
 from cldm.model import create_model, load_state_dict
+import argparse
+import os
+
+os.environ["PYTORCH_ALLOC_CONF"] = "expandable_segments:True"
+
+    # 1. Khởi tạo đối tượng đọc tham số
+parser = argparse.ArgumentParser(description="Dataset Loader cho ControlNet")
+    
+    # 2. Định nghĩa tham số --data_dir
+parser.add_argument(
+    '--data_dir', 
+    type=str, 
+    required=True, # Bắt buộc người dùng phải nhập tham số này
+    help='Đường dẫn tới thư mục chứa dataset (VD: ./training/fill50k)'
+)
+    
+    # 3. Phân tích các tham số được nhập từ terminal
+args = parser.parse_args()
+print(f"Đang tiến hành đọc dữ liệu từ: {args.data_dir}")
 
 
 # Configs
 resume_path = './models/control_sd15_ini.ckpt'
-batch_size = 4
+batch_size = 1
 logger_freq = 300
 learning_rate = 1e-5
 sd_locked = True
-only_mid_control = False
+only_mid_control = True
 
 
 # First use cpu to load models. Pytorch Lightning will automatically move it to GPUs.
@@ -25,11 +44,23 @@ model.only_mid_control = only_mid_control
 
 
 # Misc
-dataset = MyDataset()
-dataloader = DataLoader(dataset, num_workers=0, batch_size=batch_size, shuffle=True)
+dataset = MyDataset(data_dir=args.data_dir)
+print(f"Đã tải thành công {len(dataset)} mẫu dữ liệu.")
+
+dataloader = DataLoader(dataset, num_workers=2, batch_size=batch_size, shuffle=True)
 logger = ImageLogger(batch_frequency=logger_freq)
-trainer = pl.Trainer(gpus=1, precision=32, callbacks=[logger])
+trainer = pl.Trainer(
+    accelerator='gpu', 
+    devices=2, 
+    strategy="ddp", 
+    precision="16-mixed", 
+    callbacks=[logger],
+    accumulate_grad_batches=16,
+    max_epochs=15,
+    limit_train_batches=0.5
+)
 
 
 # Train!
 trainer.fit(model, dataloader)
+trainer.save_checkpoint("./models/controlnet_final_v1.ckpt")
